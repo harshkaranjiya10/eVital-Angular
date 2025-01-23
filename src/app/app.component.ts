e798b2b59dcf8c5ccd15c3d5e7c52b10e305179d
harshkaranjiya10 <harshkaranjiya10@gmail.com>
2025-01-23 14:29:26 +0530

    section 2.(>31)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { HeaderComponents } from './header/header.component';
 import { UserComponent } from './user/user.component';
-
+import { DUMMY_USERS } from './dummy-users'
+import { TaskComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponents, UserComponent],
+  imports: [HeaderComponents, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+
+
+export class AppComponent {
+  users = DUMMY_USERS
+  selectedUserId?: string;
+
+  get selectedUser() {
+    console.log(this.users.find( (user) => user.id === this.selectedUserId)!);
+    
+    return this.users.find( (user) => user.id === this.selectedUserId)!;
+  }
+  onSelectUser(id: string) {
+    console.log(id); 
+    this.selectedUserId =id;
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,12 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, signal, Input, input, Output, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const rand = Math.floor(Math.random() * DUMMY_USERS.length);
+interface user {
+  id: string,
+  name: string,
+  avatar: string
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,7 +14,7 @@ const rand = Math.floor(Math.random() * DUMMY_USERS.length);
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-/* export class UserComponent {
+export class UserComponent {
   // What we writing here is available in user.comp.html
   // And sing javascript expression we can use properties {{}}
 
@@ -19,21 +24,26 @@ const rand = Math.floor(Math.random() * DUMMY_USERS.length);
   // Supported since Angular 2
   // Checks every component and updates the relevent component.
   selectedUser = DUMMY_USERS[rand];
-
+  // Take input from parent 
+  @Input({required: true}) user!: user;
+  @Input() selected!: boolean;
+  @Output() select = new EventEmitter(); 
   // Use get keyword
   get imgPath() {
-    return `assets/users/${this.selectedUser.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
+
   onSelectUser() {
-    const rand = Math.floor(Math.random() * DUMMY_USERS.length);
-    console.log('cloclk');
-    this.selectedUser = DUMMY_USERS[rand];
+    this.select.emit(this.user.id);
+    // const rand = Math.floor(Math.random() * DUMMY_USERS.length);
+    // console.log('cloclk');
+    // this.selectedUser = DUMMY_USERS[rand]; 
   }
-} */
+} 
 
 
  // Method 2 
-export class UserComponent{
+/*  export class UserComponent{
        //---------------- 
        
        // Signal: Angular 16
@@ -50,11 +60,20 @@ export class UserComponent{
       
       // Signals not checks every components, just check the updated component and update that. 
     selectedUser = signal(DUMMY_USERS[rand]);
+    
+    // As an input from parent
+    // So why we use signals instead of implicit @Input() 
+    // We will see more @Input() in code bcz its more readable
+    // But the Signals made project fast
+    avatar = input<string>();
+    name = input<string>();
+    id = input<string>();
 
-    imgPath= computed( () => `assets/users/${this.selectedUser().avatar}`);
+    // imgPath= computed( () => `assets/users/${this.selectedUser.avatar}`);
+    imgPath= computed( () => `assets/users/${this.avatar()}`);
     onSelectUser() {
       const rand = Math.floor(Math.random() * DUMMY_USERS.length);
       //console.log('cloclk');
       this.selectedUser.set(DUMMY_USERS[rand]);
     }
-}  
+}  */  
